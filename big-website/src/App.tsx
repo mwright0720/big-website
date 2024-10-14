9bff5e9e854b01b9d712f8b71b8da00456511b18
Michael <45725311+mwright0720@users.noreply.github.com>
2024-10-14 14:35:13 -0400

    Added styling to page
    Each card is in its own little space of the page.

--- big-website/src/App.tsx
+++ big-website/src/App.tsx
@@ -1,3 +1,4 @@
+import "bulma/css/bulma.css";
 import ProfileCard from "./ProfileCard";
 import AmaneImage from "./images/amane.png";
 import ClukayImage from "./images/clukay.png";
@@ -5,17 +6,56 @@ import FischlImage from "./images/fischl.png";
 import HitagiImage from "./images/hitagi.png";
 import MarisaImage from "./images/marisa.png";
 function App() {
-    return (
-        <div>
-            <div>Some Fictional Characters</div>
-            <ProfileCard characterName="Marisa Kirisame" title="Magician" image={MarisaImage}/>
-            <ProfileCard characterName = "Hitagi Senjougahara" title="Crab" image = {HitagiImage}/>
-            <ProfileCard characterName = "Fischl" title = "Adventurer" image = {FischlImage}/>
-            <ProfileCard characterName = "Suou Amane" title = "Cook" image = {AmaneImage}/>
-            <ProfileCard characterName = "Clukay" title = "Soldier" image = {ClukayImage}/>
-        </div>
+  return (
+    <div>
+      <div>Some Fictional Characters</div>
+      <div className="container">
+        <section className="section">
+          <div className="columns">
+            <div className="column is-one-fifth">
+              <ProfileCard
+                characterName="Marisa Kirisame"
+                title="Magician"
+                image={MarisaImage}
+              />
+            </div>
 
-    );
+            <div className="column is-one-fifth">
+              <ProfileCard
+                characterName="Hitagi Senjougahara"
+                title="Crab"
+                image={HitagiImage}
+              />
+            </div>
+
+            <div className="column is-one-fifth">
+              <ProfileCard
+                characterName="Fischl"
+                title="Adventurer"
+                image={FischlImage}
+              />
+            </div>
+
+            <div className="column is-one-fifth">
+              <ProfileCard
+                characterName="Suou Amane"
+                title="Cook"
+                image={AmaneImage}
+              />
+            </div>
+
+            <div className="column is-one-fifth">
+              <ProfileCard
+                characterName="Clukay"
+                title="Soldier"
+                image={ClukayImage}
+              />
+            </div>
+          </div>
+        </section>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- big-website/src/ProfileCard.tsx
+++ big-website/src/ProfileCard.tsx
@@ -1,15 +1,23 @@
 interface Props {
   characterName: string;
-  title: string,
+  title: string;
   image: string;
 }
 
-function ProfileCard({characterName, title, image}: Props) {
+function ProfileCard({ characterName, title, image }: Props) {
   return (
-    <div>
-      <img src = {image} alt = "Image of character"></img>
-      <div>Name: {characterName}</div>
-      <div>{title}</div>
+    <div className="card">
+      <div className="card-image">
+        <figure className="image is-1by1">
+          <img src={image} alt="Image of character"></img>
+        </figure>
+      </div>
+      <div className="card-content">
+        <div className="media-content">
+          <p className="title is-4">{characterName}</p>
+          <p className="subtitle is-6">{title}</p>
+        </div>
+      </div>
     </div>
   );
 }
