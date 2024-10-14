0f6ed80899045c72e8a4866c5dc065c49847485d
Michael <45725311+mwright0720@users.noreply.github.com>
2024-10-14 14:03:35 -0400

    Added the images to the website

--- big-website/src/App.tsx
+++ big-website/src/App.tsx
@@ -1,14 +1,18 @@
 import ProfileCard from "./ProfileCard";
-
+import AmaneImage from "./images/amane.png";
+import ClukayImage from "./images/clukay.png";
+import FischlImage from "./images/fischl.png";
+import HitagiImage from "./images/hitagi.png";
+import MarisaImage from "./images/marisa.png";
 function App() {
     return (
         <div>
             <div>Some Fictional Characters</div>
-            <ProfileCard characterName="Marisa Kirisame" title="Magician"/>
-            <ProfileCard characterName = "Hitagi Senjougahara" title="Crab"/>
-            <ProfileCard characterName = "Fischl" title = "Adventurer"/>
-            <ProfileCard characterName = "Suou Amane" title = "Cook"/>
-            <ProfileCard characterName = "Clukay" title = "Soldier"/>
+            <ProfileCard characterName="Marisa Kirisame" title="Magician" image={MarisaImage}/>
+            <ProfileCard characterName = "Hitagi Senjougahara" title="Crab" image = {HitagiImage}/>
+            <ProfileCard characterName = "Fischl" title = "Adventurer" image = {FischlImage}/>
+            <ProfileCard characterName = "Suou Amane" title = "Cook" image = {AmaneImage}/>
+            <ProfileCard characterName = "Clukay" title = "Soldier" image = {ClukayImage}/>
         </div>
 
     );
--- big-website/src/ProfileCard.tsx
+++ big-website/src/ProfileCard.tsx
@@ -1,11 +1,13 @@
 interface Props {
   characterName: string;
-  title: string;
+  title: string,
+  image: string;
 }
 
-function ProfileCard({characterName, title}: Props) {
+function ProfileCard({characterName, title, image}: Props) {
   return (
     <div>
+      <img src = {image} alt = "Image of character"></img>
       <div>Name: {characterName}</div>
       <div>{title}</div>
     </div>
